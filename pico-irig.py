945993891462f209a687c683d861873b4a551c50
Simon Wood <simon@mungewell.org>
2024-12-27 11:23:39 -0700

    Optimized FIFO statemachine

--- pico-irig.py
+++ pico-irig.py
@@ -1,3 +1,35 @@
+# Pico-Irig for Raspberry-Pi Pico
+# (c) 2024-12-26 Simon Wood <simon@mungewell.org>
+#
+# https://github.com/mungewell/pico-irig
+#
+# MIT license - go make something cool....
+#
+# Basis is that High/Low cycles will be formed with the hack for
+# different output levels from (ab)using the internal Pull-Up/-Downs.
+#
+# External resistors set the relative amplitude of the high vs low cycle.
+#
+# High Cycle:
+#     set(pindirs, 0b11) .side(0b01) [1]		# Zero = Low, High
+#     set(pindirs, 0b11) .side(0b11) [3]		# High = High, High
+#     set(pindirs, 0b11) .side(0b01) [1]		# Zero = Low, High
+#     set(pindirs, 0b11) .side(0b00) [3]		# Low  = Low, Low
+#
+# Low Cycle:
+#     set(pindirs, 0b11) .side(0b01) [1]		# Zero = Low, High
+#     set(pindirs, 0b01) .side(0b01) [3]		# high = In-pull Low, High
+#     set(pindirs, 0b11) .side(0b01) [1]		# Zero = Low, High
+#     set(pindirs, 0b10) .side(0b00) [3]		# low  = Low, In-Pull High
+#
+# Each symbol takes 12 PIO clocks (at 12KHz for IRIG-B).
+#
+# The 'current symbol' is passed via the FIFO with 2bits per symbol, these
+# are 'extracted' by a separate/synchronized SM and placed on GPIO pins:
+#     0b00 - Data '0'
+#     0b01 - Data '1'
+#     0b10 - Pr, P1..P9, P0
+
 import rp2
 import utime
 from machine import Pin, disable_irq, enable_irq, mem32, freq, I2C
@@ -22,65 +54,13 @@ irig_trigger = IRIG_FAKE
 
 IRIG_PPS_RISING = 0
 IRIG_PPS_FALLING = 1
-irig_polarity = IRIG_PPS_FALLING
+irig_polarity = IRIG_PPS_RISING
 
 # globals
 irig_sm = []
 irig_fifo = []
 irig_seconds = 0.0
-
-sync_ticks_us = 0
-pps_ticks_us = 0
-
-fail = 0
-core_dis = [0, 0]
-
-
-'''
-Basis is that High/Low cycles will be formed with the hack for
-different output levels from (ab)using the internal Pull-Up/-Downs.
-
-External resistors set the relative amplitude of the low cycle.
-
-High Cycle:
-    set(pindirs, 0b11) .side(0b01) [1]		# Zero = Low, High
-    set(pindirs, 0b11) .side(0b11) [3]		# High = High, High
-    set(pindirs, 0b11) .side(0b01) [1]		# Zero = Low, High
-    set(pindirs, 0b11) .side(0b00) [3]		# Low  = Low, Low
-
-Low Cycle:
-    set(pindirs, 0b11) .side(0b01) [1]		# Zero = Low, High
-    set(pindirs, 0b01) .side(0b01) [3]		# high = In-pull Low, High
-    set(pindirs, 0b11) .side(0b01) [1]		# Zero = Low, High
-    set(pindirs, 0b10) .side(0b00) [3]		# low  = Low, In-Pull High
-
-
-Each cycle takes 12 PIO clocks, and we'll reduce the 'nop()'s to
-allow for other required instructions/op-codes
-
-The 'requested cycle' is passed via the FIFO with 2bits per bit, these
-are 'extracted' by a separate/synchronized SM and placed on pins:
-    0b00 - Data '0'
-    0b01 - Data '1'
-    0b10 - Pr, P1..P9, P0
-'''
-
-
-@rp2.asm_pio(autopull=True,
-             fifo_join=rp2.PIO.JOIN_TX, out_shiftdir=rp2.PIO.SHIFT_RIGHT)
-
-def setup():
-    #irq(clear, rel(0))
-    irq(4)
-    out(null, 32)
-    out(null, 32)
-    out(null, 32)
-    out(null, 32)
-    out(null, 32)
-    out(null, 32)
-    out(null, 32)
-    out(null, 32)
-
+irig_fail = 0
 
 @rp2.asm_pio()
 
@@ -88,7 +68,7 @@ def start_from_pin_rising():
     wait(0, pin, 0)
     wait(1, pin, 0)
 
-    irq(clear, 4)					# Trigger Sync
+    irq(4)      					# Trigger Sync
     #irq(rel(0))					# set IRQ for ticks_us monitoring
 
 
@@ -98,51 +78,86 @@ def start_from_pin_falling():
     wait(1, pin, 0)
     wait(0, pin, 0)
 
-    irq(clear, 4)					# Trigger Sync
+    irq(4)      					# Trigger Sync
     #irq(rel(0))					# set IRQ for ticks_us monitoring
 
 
-@rp2.asm_pio(out_init=[rp2.PIO.OUT_LOW] * 2, autopull=True,
+# Purge the TX-FIFO and preset the interrupts/X-register
+
+@rp2.asm_pio(autopull=True,
+             fifo_join=rp2.PIO.JOIN_TX, out_shiftdir=rp2.PIO.SHIFT_RIGHT)
+
+def irig_fifo_purge():
+    irq(clear, rel(0))
+    irq(clear, 4)
+    set(x, 8)					    # FIFO 100 bits = (9 * 11) + 1
+
+    wrap_target()
+    out(null, 2)
+    wrap()
+
+
+# Optimized implementation - only error check 1st 'Out()' of next frame(s),
+# as you'd have to be daft to start without filling the first frame.
+# Runs at reduced clock = irig_clk * 2 = 2Khz (20 steps per symbol)
+
+@rp2.asm_pio(out_init=[rp2.PIO.OUT_LOW, rp2.PIO.OUT_HIGH], autopull=True,
              fifo_join=rp2.PIO.JOIN_TX, out_shiftdir=rp2.PIO.SHIFT_RIGHT)
 
 def irig_fifo():
-    set(x, 11-2)					# FIFO holds 11x Pr/P1..0, which we count out
+    irq(clear, 4)
+    set(x, 8)					    # FIFO 100 bits = (9 * 11) + 1
+
     out(pins, 2)					# Preload first bit-pair for better timing
-    jmp(not_osre, "pre-load-done")
-    jmp("fail")
- 
-    label("pre-load-done")
-    irq(block, 4)					# Wait for Sync'ed start
- 
-    label("start-of-frame")
-    irq(rel(0))                     # set IRQ for ticks_us monitoring
-    label("loop")
-    set(y, 23-1)					# delay = (12 clocks * 10 cycles) - 5
-    label("delay")
-    jmp(y_dec, "delay") [4]
- 
-    out(pins, 2)					# output next bit-pair
-    jmp(not_osre, "continue")
- 
-    label("fail")					# UNDERFLOW - when Python fails to fill FIFOs
-    irq(rel(0))                     # set IRQ to warn other StateMachines
-    wrap_target()
+
+    wait(1, irq, 4)					# Wait for Sync'ed start, then clear IRQ
+
+    label("outer")
+    set(y, 9) [17]                  # (loop for 10) + 1 extra = 11
+
+    label("inner")
+    out(pins, 2)					# output bit-pair
+    jmp(y_dec, "inner") [18]
+
+    out(pins, 2)					# output extra/last bit-pair
+    jmp(x_dec, "outer")
+
+    set(x, 8) [16]
+    out(null, 24)     				# clear out unused-bits section of ISR
+    out(pins, 2)					# output first bit-pair of frame
+    jmp(not_osre, "outer")
+
+    irq(rel(0))                     # UNDERFLOW - when Python fails to fill FIFOs
+    wrap_target()                   # set IRQ to warn other StateMachines
     set(pins, 0)
     wrap()
- 
-    label("continue")
-    jmp(pin, "px_frame")			# check HIGH=Pr/P1..0, and count them...
-    jmp("loop")
-    label("px_frame")
-    jmp(x_dec, "loop")
 
-    nop() [30]						# padding to (12 clocks * 10 cycles) - 7
-    nop() [30]
-    out(null, 24) [30]				# clear out unused-bits section of ISR
-    set(x, 11-2) [23]				# FIFO holds 11x Pr/P1..0
-    out(pins, 2)					# output first bit-pair of frame
-    jmp(not_osre, "start-of-frame")
-    jmp("fail")
+
+# Minimal implementation - don't check for Underflow error
+# Requires 'purge' to set X first
+
+@rp2.asm_pio(out_init=[rp2.PIO.OUT_LOW, rp2.PIO.OUT_HIGH], autopull=True,
+             fifo_join=rp2.PIO.JOIN_TX, out_shiftdir=rp2.PIO.SHIFT_RIGHT)
+
+def irig_fifo_minimal():
+    out(pins, 2)					# Preload first bit-pair for better timing
+    wait(1, irq, 4)					# Wait for Sync'ed start, then clear IRQ
+
+    wrap_target()
+    label("outer")
+    set(y, 9) [17]                  # (loop for 10) + 1 extra = 11
+
+    label("inner")
+    out(pins, 2)					# output bit-pair
+    jmp(y_dec, "inner") [18]
+
+    out(pins, 2)					# output extra/last bit-pair
+    jmp(x_dec, "outer")
+
+    set(x, 8) [16]
+    out(null, 24)     				# clear out unused-bits section of ISR
+    out(pins, 2) [1]                # output first bit-pair of frame
+    wrap()
 
 
 @rp2.asm_pio(out_init=[rp2.PIO.OUT_HIGH])
@@ -159,7 +174,9 @@ def irig_enc():
     set(x, 3)
     set(y, 2)
  
-    irq(block, 4) [2]  						# Wait for Sync'ed start
+    wait(1, irq, 4)                         # Wait for Sync'ed start, then clear IRQ
+                                            # race condition
+
     wrap()									# abuse wrap for PPS sync
 
     label("start-of-symbol")				# loop for 2 x 12 clocks
@@ -197,7 +214,8 @@ def irig_enc():
          set_init=[rp2.PIO.IN_HIGH, rp2.PIO.IN_LOW])
 
 def irig_ask():
-    irq(block, 4) [1]						# Wait for Sync'ed start
+    wait(1, irq, 4)                         # Wait for Sync'ed start, then clear IRQ
+                                            # race condition
  
     # fall through for faster PPS sync
 
@@ -226,30 +244,6 @@ def sync_sm(r0, r1):
     lsl(r2, r3)
     str(r2, [r0, 0])
     str(r2, [r1, 0])
-
-def irq_handler(m):
-    global core_dis, stop, irig_sm, fifo
-    global sync_ticks_us, pps_ticks_us
-
-    core_dis[mem32[0xd0000000]] = disable_irq()
-    ticks = utime.ticks_us()
-
-    if m==irig_sm[1]:
-        # reset the clock-phases with CLKDIV_RESTART
-        mem32[0x50300000] = 0x00000f00		# Block-2 first as more critical
-        mem32[0x50200000] = 0x00000f00
- 
-        sync_ticks_us = ticks
-
-    if m==irig_sm[3]:
-        pps_ticks_us = ticks
-
-    if m==irig_sm[fifo]:
-        # Buffer Underflow
-        fail = 1
-
-    enable_irq(core_dis[mem32[0xd0000000]])
-
 
 # -----
 packed = []
@@ -376,16 +370,22 @@ if __name__ == "__main__":
     # configure the PPS pin
     pps = machine.Pin(18, machine.Pin.IN, machine.Pin.PULL_UP)
  
+    if irig_trigger == IRIG_RTC:
+        # Start the StateMachines using a 1PPS signal
+        # (for now using a RTC chip as our 1PPS reference)
+        ds = DS3231(I2C(0, sda=Pin(16), scl=Pin(17)))
+        ds.square_wave(freq=ds.FREQ_1)
+
     # setup the StateMachines, ensuring FIFO is empty and IRQ set
     irig_sm = []
-    irig_sm.append(rp2.StateMachine(0, setup, freq=ext_freq))
-    irig_sm.append(rp2.StateMachine(1, setup, freq=ext_freq))
-    irig_sm.append(rp2.StateMachine(2, setup, freq=ext_freq))
-    irig_sm.append(rp2.StateMachine(3, setup, freq=ext_freq))
-    irig_sm.append(rp2.StateMachine(4, setup, freq=ext_freq))
-    irig_sm.append(rp2.StateMachine(5, setup, freq=ext_freq))
-    irig_sm.append(rp2.StateMachine(6, setup, freq=ext_freq))
-    irig_sm.append(rp2.StateMachine(7, setup, freq=ext_freq))
+    irig_sm.append(rp2.StateMachine(0, irig_fifo_purge, freq=ext_freq))
+    irig_sm.append(rp2.StateMachine(1, irig_fifo_purge, freq=ext_freq))
+    irig_sm.append(rp2.StateMachine(2, irig_fifo_purge, freq=ext_freq))
+    irig_sm.append(rp2.StateMachine(3, irig_fifo_purge, freq=ext_freq))
+    irig_sm.append(rp2.StateMachine(4, irig_fifo_purge, freq=ext_freq))
+    irig_sm.append(rp2.StateMachine(5, irig_fifo_purge, freq=ext_freq))
+    irig_sm.append(rp2.StateMachine(6, irig_fifo_purge, freq=ext_freq))
+    irig_sm.append(rp2.StateMachine(7, irig_fifo_purge, freq=ext_freq))
     for m in range(len(irig_sm)):
         irig_sm[m].active(1)
     utime.sleep(0.5)
@@ -394,29 +394,25 @@ if __name__ == "__main__":
  
     # On PIO Block-1
     irig_sm = []
+    fifo_sm = len(irig_sm)
+    irig_sm.append(rp2.StateMachine(2, irig_fifo, freq=irig_freq * 2, \
+                        out_base=Pin(3), jmp_pin=Pin(4)))
+    '''
+    irig_sm.append(rp2.StateMachine(2, irig_fifo_minimal, freq=irig_freq * 2, \
+                        out_base=Pin(3), jmp_pin=Pin(4)))
+    '''
+
     if irig_polarity == IRIG_PPS_RISING:
         irig_sm.append(rp2.StateMachine(0, start_from_pin_rising, freq=ext_freq, \
-                            in_base=Pin(18), jmp_pin=Pin(18)))
-        irig_sm.append(rp2.StateMachine(1, start_from_pin_rising, freq=cpu_freq, \
                             in_base=Pin(18), jmp_pin=Pin(18)))
     else:
         irig_sm.append(rp2.StateMachine(0, start_from_pin_falling, freq=ext_freq, \
                             in_base=Pin(18), jmp_pin=Pin(18)))
-        irig_sm.append(rp2.StateMachine(1, start_from_pin_falling, freq=cpu_freq, \
-                            in_base=Pin(18), jmp_pin=Pin(18)))
-    fifo_sm = len(irig_sm)
-    irig_sm.append(rp2.StateMachine(2, irig_fifo, freq=irig_freq * 12, \
-                        out_base=Pin(3), jmp_pin=Pin(4)))
-    irig_sm.append(rp2.StateMachine(3, irig_dcls, freq=irig_freq * 12, \
+
+    irig_sm.append(rp2.StateMachine(1, irig_dcls, freq=irig_freq * 12, \
                         in_base=Pin(5), out_base=Pin(6)))
 
     # On PIO Block-2
-    if irig_polarity == IRIG_PPS_RISING:
-        irig_sm.append(rp2.StateMachine(4, start_from_pin_rising, freq=ext_freq, \
-                            in_base=Pin(18), jmp_pin=Pin(18)))
-    else:
-        irig_sm.append(rp2.StateMachine(4, start_from_pin_falling, freq=ext_freq, \
-                            in_base=Pin(18), jmp_pin=Pin(18)))
     irig_sm.append(rp2.StateMachine(5, irig_enc, freq=irig_freq * 12, \
                         set_base=Pin(5), in_base=Pin(3), \
                         jmp_pin=Pin(4)))
@@ -424,27 +420,25 @@ if __name__ == "__main__":
                         sideset_base=Pin(0), set_base=Pin(0), \
                         jmp_pin=Pin(5)))
 
-    '''
-    # set up IRQ handler(s)
-    for m in irig_sm:
-        m.irq(handler=irq_handler, hard=True)
-    '''
-    
-    # pre-set the IRQ4 on each SM with IRQ_FORCE
-    mem32[0x50200034] = 1 << 4
-    mem32[0x50300034] = 1 << 4
- 
+    if irig_polarity == IRIG_PPS_RISING:
+        irig_sm.append(rp2.StateMachine(4, start_from_pin_rising, freq=ext_freq, \
+                            in_base=Pin(18), jmp_pin=Pin(18)))
+    else:
+        irig_sm.append(rp2.StateMachine(4, start_from_pin_falling, freq=ext_freq, \
+                            in_base=Pin(18), jmp_pin=Pin(18)))
+
     # re-align the clock-phases with CLKDIV_RESTART
     sync_sm(0x50300000, 0x50200000)          # Block-2 first as more timing critical
 
     # Pre-start most StateMachines
+    mem32[0x50200000] = 0x0000000e
     mem32[0x50300000] = 0x0000000e
-    mem32[0x50200000] = 0x00000004
 
     # Pre-fill the entry in FIFO
     if irig_sm[fifo_sm].tx_fifo() < 1:
-        #pack_test()
-        pack_from_seconds(irig_seconds)
+        pack_test()
+        #pack_from_seconds(irig_seconds)
+
         for p in irig_fifo:
             irig_sm[fifo_sm].put(p)
         irig_seconds += (1000 / irig_freq)
@@ -453,31 +447,17 @@ if __name__ == "__main__":
     utime.sleep(5)
  
     # ---
-    # Test section: True for fake, False for RTC
+    # Test section: 
+    # Enable the rest of StateMachines
+    mem32[0x50200000] = 0x0000000f
+    mem32[0x50300000] = 0x0000000f
+    print("Go...")
+
     if irig_trigger == IRIG_FAKE:
-        # Enable the rest of StateMachines
-        mem32[0x50200000] = 0x0000000d
-        mem32[0x50300000] = 0x0000000f
- 
-        utime.sleep(0.2)
- 
         # Start the StateMachines asserting (fake) 1PPS low
         pps = machine.Pin(18, machine.Pin.OUT, value=0)
         utime.sleep(0.1)
         pps = machine.Pin(18, machine.Pin.IN, machine.Pin.PULL_UP)
-    else:
-        # Start the StateMachines using a 1PPS signal
-        # (for now using a RTC chip as our 1PPS reference)
-        ds = DS3231(I2C(0, sda=Pin(16), scl=Pin(17)))
-        ds.square_wave(freq=ds.FREQ_1)
-
-        while pps.value() == 1:
-            # wait for PPS to deassert first, as some are 1Hz signals...
-            utime.sleep(0.25)
- 
-        # Enable the rest of StateMachines
-        mem32[0x50200000] = 0x0000000f
-        mem32[0x50300000] = 0x0000000f
     # ---
  
     # wait for the trigger to have happen
@@ -487,14 +467,15 @@ if __name__ == "__main__":
     # Loop, filling the FIFO as needed
     print("IRIG running...")
     count = 0
-    while not fail:
+    while not irig_fail:
         if irig_sm[fifo_sm].tx_fifo() < 1:
+            '''
             pack_from_seconds(irig_seconds)
             irig_seconds += (1000 / irig_freq)
             '''
             pack_test(count)
             count = (count + 1) & 0xFF
-            '''
+
             for p in irig_fifo:
                 irig_sm[fifo_sm].put(p)
             print(".", end="")
